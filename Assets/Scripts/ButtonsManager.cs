bf1844f3ffeb9b3f17145c0751b93a8c0ac21a06
Eddy <volkikushaytnogi@gmail.com>
2024-03-10 17:11:45 +0100

    important fix!
    added buttons manager and settings button

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonsManager : MonoBehaviour
+{
+    [SerializeField] Button startRandomingButton;
+    [SerializeField] Button settingsButton;
+
+    /* что нужно сделать:
+
+    нужен метод, который делает все кнопки на экране неактивными.
+    на момент у нас их всего 2, гого рандомить и настройки, но кнопки могут в будущем и добавиться
+    реализовано по идее это должно быть не костылем, т.е. вариант в котором мы отключение каждой кнопки руками прописываем отдельно
+    нам явно не подходит
+    офк не забывай про комменты, стейдж, коммит, пуш
+
+    */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
